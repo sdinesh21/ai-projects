2f12ebe5fcfaa4d7566462e64653ddcf3d88d2d2
sdinesh21 <61404247+sdinesh21@users.noreply.github.com>
2024-02-11 12:36:46 -0600

    Update app.py

--- langgraph-agent/app.py
+++ langgraph-agent/app.py
@@ -145,12 +145,10 @@ def main():
       inputs = {"input": input_text, "chat_history": [], "return_direct": False}
       results = []
       for s in app.stream(inputs):
-          <!-- 
-          result = list(s.values())[0]
-          results.append(result)
+          # result = list(s.values())[0]
+          # results.append(result)
           st.write(result)
           result = app.invoke({"input": input_text, "chat_history": [], "return_direct": False})
-          -->
           print(result["agent_outcome"].return_values["output"])
     
 if __name__ == "__main__":
